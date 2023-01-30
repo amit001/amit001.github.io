bccfe9bf40effdf06040d5874ceba43e7313fe0d
Amit <amit@delimp.com>
2023-01-30 18:49:08 +0530

    publicly publish

---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,73 @@
+import { Footer } from 'components/Footer';
+import { Meta } from 'components/Meta';
+import { Intro } from 'layouts/Home/Intro';
+import { Profile } from 'layouts/Home/Profile';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Home.module.css';
+
+const disciplines = ['TypeScript', 'Laravel', 'ReactJs', 'NodeJs', 'MongoDb'];
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Meta
+        title="Designer + Developer"
+        description="Design portfolio of AMIT KUMAR — a product designer working on web & mobile
+          apps with a focus on motion, experience design, and accessibility."
+      />
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        disciplines={disciplines}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
